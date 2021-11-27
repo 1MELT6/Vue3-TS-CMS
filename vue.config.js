1127dbebf493194f0a818f08c6265b63ab6596ba
melting <meltcahaeng@gmail.com>
2021-11-27 17:40:37 +0800

    feat: add elementplus glabal

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-const path = require('path')
+// const path = require('path')
 
 module.exports = {
   // 1.配置方式一: CLI提供的属性
   outputDir: './build',
-  publicPath: './',
+  publicPath: './'
   // 2.配置方式二: 和webpack属性完全一致, 最后会进行合并
   // configureWebpack: {
   //   resolve: {
@@ -19,9 +19,9 @@ module.exports = {
   //   }
   // }
   // 3.配置方式三:
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set('@', path.resolve(__dirname, 'src'))
-      .set('components', '@/components')
-  }
+  //   chainWebpack: (config) => {
+  //     config.resolve.alias
+  //       .set('@', path.resolve(__dirname, 'src'))
+  //       .set('components', '@/components')
+  //   }
 }
