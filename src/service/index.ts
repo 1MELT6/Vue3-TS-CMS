955b460a7cfdf09a42e8251b7098fa95c61b76e4
melting <meltcahaeng@gmail.com>
2021-11-28 16:59:35 +0800

    feat: has more than one request instance how to accomplish

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,4 +1,14 @@
 import HYRequest from './request'
 //全放在对象不好
 // export default hyRequest = {}
-export default new HYRequest()
+
+const hyRequest = new HYRequest({
+  baseURL: '地址'
+})
+
+// 多个base地址不同：就是多个实例对象的创建
+export const hyRequest2 = new HYRequest({
+  baseURL: '地址'
+})
+
+export default hyRequest
--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,4 +1,13 @@
+import axios from 'axios'
+import type { AxiosInstance, AxiosRequestConfig } from 'axios'
 class HYRequest {
+  // instance: any
+  instance: AxiosInstance
+  // 创建出不同的实例保存到instance
+  constructor(config: AxiosRequestConfig) {
+    this.instance = axios.create(config)
+  }
+
   // request() {}
   // get() {}
 }
