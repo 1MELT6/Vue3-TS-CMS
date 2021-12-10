0a9ed4b306f44982f97f6ccaca9298aa817c91fa
melting <meltcahaeng@gmail.com>
2021-12-10 23:17:38 +0800

    feat: login validate

--- src/views/login/cpns/login-accout.vue
+++ src/views/login/cpns/login-accout.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form label-width="60px" :rules="rules" :model="account">
+  <el-form label-width="60px" :rules="rules" :model="account" ref="formRef">
     <!-- 不加：model会识别不到限制5-10个字符 -->
     <el-form-item label="账号" prop="name">
       <el-input v-model="account.name" />
@@ -12,8 +12,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import { rules } from '../config/account-config'
+import { ElForm } from 'element-plus'
 export default defineComponent({
   // props: {
   //   account: {
@@ -51,7 +52,15 @@ export default defineComponent({
       name: '',
       password: ''
     })
-    return { account, rules }
+    //  全局引用elform需要另外导入
+    const formRef = ref<InstanceType<typeof ElForm>>()
+    // 从handlelogin过来
+    const LoginAction = () => {
+      formRef.value?.validate((valid) => {
+        console.log(valid) //验证失败返回false
+      })
+    }
+    return { account, rules, LoginAction, formRef }
   }
 })
 </script>
--- src/views/login/cpns/login-panel.vue
+++ src/views/login/cpns/login-panel.vue
@@ -8,7 +8,7 @@
             <i class="el-icon-user-solid">账号登陆</i>
           </span>
         </template>
-        <LoginAccout />
+        <LoginAccout ref="accountRef" />
       </el-tab-pane>
       <el-tab-pane>
         <template #label>
@@ -20,15 +20,17 @@
       </el-tab-pane>
     </el-tabs>
     <div class="account-control">
-      <el-checkbox>记住密码</el-checkbox>
+      <el-checkbox v-model="isKeepPassword">记住密码</el-checkbox>
       <el-link type="primary">忘记密码</el-link>
     </div>
-    <el-button type="primary" class="loginButton">登录</el-button>
+    <el-button type="primary" class="loginButton" @click="handleLoginClick"
+      >登录</el-button
+    >
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import LoginPhone from './login-phone.vue'
 import LoginAccout from './login-accout.vue'
 export default defineComponent({
@@ -37,7 +39,17 @@ export default defineComponent({
     LoginAccout
   },
   setup() {
-    return {}
+    // 默认打勾
+    const isKeepPassword = ref(true)
+    //不默认为空可以执行真正登录,但是需要传泛型是loginaccount但是他是个对象
+    const accountRef = ref<InstanceType<typeof LoginAccout>>() //默认为空然后返回再进行绑定logonaccount
+    // 大的告诉小的执行操作：直接拿组件对象ref
+    const handleLoginClick = () => {
+      console.log('立即登录', accountRef.value) //目的是执行account组件的（accountRef.value.loginaction）
+      accountRef.value?.LoginAction()
+      // 保证loginAction写错是就标红不用编译运行
+    }
+    return { handleLoginClick, isKeepPassword, accountRef }
   }
 })
 </script>
