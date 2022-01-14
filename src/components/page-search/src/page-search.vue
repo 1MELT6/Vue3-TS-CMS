5d61f702f1669ffda6030cd27cf42706dc979940
melting <meltcahaeng@gmail.com>
2022-01-14 17:50:27 +0800

    feat: page-search

---
+++ src/components/page-search/src/page-search.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="page-search">
+    <hy-form v-bind="searchFormConfig" v-model="formData">
+      <template #header>
+        <h1 class="header">高级检索</h1>
+      </template>
+      <template #footer>
+        <div class="handle-btns">
+          <el-button type="default" icon="el-icon-refresh">重置</el-button>
+          <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        </div>
+      </template>
+    </hy-form>
+  </div>
+</template>
+<script lang="ts">
+import HyForm from '@/base-ui/form'
+
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  props: {
+    searchFormConfig: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    HyForm
+  },
+  setup() {
+    // ref对双向绑定更兼容
+    const formData = ref({
+      id: '',
+      name: '',
+      password: '',
+      sport: '',
+      createTime: ''
+    })
+    return {
+      formData
+    }
+  }
+})
+</script>
+<style scoped>
+.header {
+  color: red;
+}
+.handle-btns {
+  text-align: right;
+  padding: 0px 50px 20px 0px;
+}
+</style>
--- src/views/main/user/user.vue
+++ src/views/main/user/user.vue
@@ -1,43 +1,21 @@
 <template>
   <div class="user">
-    <hy-form v-bind="searchFormConfig" v-model="formData">
-      <template #header>
-        <h1 class="header">高级检索</h1>
-      </template>
-      <template #footer>
-        <div class="handle-btns">
-          <el-button type="default" icon="el-icon-refresh">重置</el-button>
-          <el-button type="success" icon="el-icon-search">搜索</el-button>
-        </div>
-      </template>
-    </hy-form>
+    <page-search :searchFormConfig="searchFormConfig" />
     <div class="content"></div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import HyForm from '@/base-ui/form'
-
+import { defineComponent } from 'vue'
+import pageSearch from '@/components/page-search/src/page-search.vue'
 import { searchFormConfig } from './config/search.config'
 
 export default defineComponent({
   name: 'user',
-  components: {
-    HyForm
-  },
+  components: { pageSearch },
   setup() {
-    // ref对双向绑定更兼容
-    const formData = ref({
-      id: '',
-      name: '',
-      password: '',
-      sport: '',
-      createTime: ''
-    })
     return {
-      searchFormConfig,
-      formData
+      searchFormConfig
     }
   }
 })
